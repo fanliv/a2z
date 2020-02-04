38812e00b8e2bb5228b0c3c8264eb09455725c29
luffyacekun <48741191+luffyacekun@users.noreply.github.com>
2020-02-04 11:20:11 +0700

    fresh start

---
+++ app/src/main/java/com/cyborg/a2z/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.cyborg.a2z
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.cyborg.a2z.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+
+}
